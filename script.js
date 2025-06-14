fa957f550875209cfdfc4e4cccec3dfb625f7662
ivan <ivan.montero@csfacturacion.com>
2025-06-14 03:16:57 -0600

    Agregando archivos del proyecto

---
+++ script.js
@@ -0,0 +1,15 @@
+// Instrucciones:
+// 1. Agrega un listener al botón con id="btnCargar"
+// 2. Cuando se haga clic, usa fetch para obtener todos los personajes de la API:
+//    https://rickandmortyapi.com/api/character
+// 3. Filtra solo aquellos con species = "Human" y status = "Alive"
+// 4. Por cada uno, crea una tarjeta similar al ejemplo en el HTML y agrégala al contenedor con id="galeria"
+// 5. BONUS: Implementa bubbling en las tarjetas, de modo que al hacer clic en una tarjeta muestre el nombre del personaje en consola
+
+// Punto de partida (pueden usarlo):
+const btn = document.getElementById('btnCargar');
+const galeria = document.getElementById('galeria');
+
+btn.addEventListener('click', async () => {
+  // Aquí comienza tu código...
+});
